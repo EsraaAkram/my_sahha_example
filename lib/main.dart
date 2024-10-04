624854fd435fb1a864fc85ece8a674e201b5766d
EsraaAkram <totalappsingplay@gmail.com>
2024-10-04 17:43:46 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:sahha_flutter/sahha_flutter.dart';
+// import 'package:sahha_flutter_example/Views/ScoresView.dart';
+// import 'package:sahha_flutter_example/Views/AuthenticationView.dart';
+// import 'package:sahha_flutter_example/Views/HomeView.dart';
+// import 'package:sahha_flutter_example/Views/ProfileView.dart';
+// import 'package:sahha_flutter_example/Views/WebView.dart';
+// import 'package:sahha_flutter_example/Views/SensorPermissionView.dart';
+
+import 'Views/AuthenticationView.dart';
+import 'Views/HomeView.dart';
+import 'Views/ProfileView.dart';
+import 'Views/ScoresView.dart';
+import 'Views/SensorPermissionView.dart';
+import 'Views/WebView.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => AppState();
+}
+
+class AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Use default values
+    SahhaFlutter.configure(
+      environment: SahhaEnvironment.sandbox,
+    )
+        .then((success) => {debugPrint(success.toString())})
+        .catchError((error, stackTrace) => {debugPrint(error.toString())});
+
+/*
+    // Android only
+    var notificationSettings = {
+      'icon': 'Custom Icon',
+      'title': 'Custom Title',
+      'shortDescription': 'Custom Description'
+    };
+
+    // Use custom values
+    SahhaFlutter.configure(
+            environment: SahhaEnvironment.production,
+            sensors: [SahhaSensor.device],
+            notificationSettings: notificationSettings)
+        .then((success) => {debugPrint(success.toString())})
+        .catchError((error, stackTrace) => {debugPrint(error.toString())});
+        */
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => const HomeView(),
+        '/authentication': (BuildContext context) => const AuthenticationView(),
+        '/profile': (BuildContext context) => const ProfileView(),
+        '/permissions': (BuildContext context) => const SensorPermissionView(),
+        '/scores': (BuildContext context) => const ScoresView(),
+        '/web': (BuildContext context) => const WebView(),
+      },
+    );
+  }
+}
